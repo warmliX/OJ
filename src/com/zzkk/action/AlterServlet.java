d19b521440a265e7b79161e043a171360fac60ee
warmli <32098223+warmliX@users.noreply.github.com>
2020-10-05 17:39:56 +0800

    tt
    tt

--- src/com/zzkk/action/AlterServlet.java
+++ src/com/zzkk/action/AlterServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.SQLException;
-
+//
 public class AlterServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String email = request.getParameter("name");
