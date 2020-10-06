03eae783f647d0a860ef3e1f81ee7dce7e1109af
warmli <32098223+warmliX@users.noreply.github.com>
2020-10-06 09:29:14 +0800

    tt
    tt

--- src/com/zzkk/bean/UserBean.java
+++ src/com/zzkk/bean/UserBean.java
@@ -6,7 +6,7 @@ public class UserBean {
     private String password;
     private String uquestion;
     private String uanswer;
-
+////
     public String getEmail() {
         return email;
     }
